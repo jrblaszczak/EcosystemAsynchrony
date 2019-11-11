ab8c8e0aa06ceacb1a381128530bcfc2fd304870
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-11-11 10:18:44 -0700

    Added a few notes to dtw

--- dtw_analysis.R
+++ dtw_analysis.R
@@ -1,5 +1,6 @@
-# Do dynamic time warping
+# Do dynamic time warping on discharge data in the Boise River Basin
 
+#change this path to your local path name - there is a better way than switching everytime but that will take me some time to figure out
 setwd("Users/kek25/Documents/GitRepos/EcosystemAsynchrony/Data/")
 
 library(dtw)
@@ -8,7 +9,7 @@ library(tidyverse)
 Q=read.csv("BRB_Q_wy2019_merged.csv")
 Q=Q[6:4353, ]
 
-
+# need this for plotting
 alignment<-dtw(Q$TwinSprings.Q,Q$Glenwood.Q,keep=TRUE)
 
 ## Display the warping curve, i.e. the alignment curve
@@ -19,12 +20,14 @@ plot(
       step=rabinerJuangStepPattern(6,"c")),
   type="twoway",offset=-2)
 
-## See the recursion relation, as formula and diagram
+## See the recursion relation, as formula and diagram - I dont understand these, but they mean something
 rabinerJuangStepPattern(6,"c")
 plot(rabinerJuangStepPattern(6,"c"))
 
-#TwnSprings - Glenwood
 
+#now do the same thing for other pairs of gages
+
+#TwnSprings - Glenwood
 plot(
   dtw(Q$TwnSprings.Q, Q$Parma.Q,keep=TRUE,
       step=rabinerJuangStepPattern(6,"c")),
@@ -41,7 +44,7 @@ plot(
       step=rabinerJuangStepPattern(6,"c")),
   type="twoway",offset=-2)
 
-
+#I dont remember what all this does, but it ran really slow on my comp ...
 lcm <- alignment$localCostMatrix
 image(x = 1:nrow(lcm), y = 1:ncol(lcm), lcm)
 text(row(lcm), col(lcm), label = lcm)
